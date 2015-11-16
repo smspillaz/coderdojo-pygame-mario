78034b01a75355f910587101d4379718ed3798bb
Sam Spilsbury <smspillaz@gmail.com>
2015-11-16 14:26:24 +0800

    Create TileGrid and load all the images

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ import sys
 pygame.display.set_caption("Super Mario Test")
 screen = pygame.display.set_mode((640, 480))
 
+class TileGrid(object):
+    def __init__(self, screen):
+        super(TileGrid, self).__init__()
+        screen_size = screen.get_size()
+        self.grid_width = screen_size[0] // 16
+        self.grid_height = screen_size[1] // 16
+        self.grid = [None for i in range(0, self.grid_width * self.grid_height)]
+
+        self.left_tile_image, self.left_tile_rect = load_image("ground_tiles_1_1.png")
+        self.center_tile_image, self.center_tile_rect = load_image("ground_tiles_1_2.png")
+        self.right_tile_image, self.right_tile_rect = load_image("ground_tiles_1_3.png")
+        self.ground_tile_image, self.ground_tile_rect = load_image("ground_tiles_1_4.png")
+
+
+
 def make_background(image_name, screen):
     background, background_rect = load_image(image_name)
     background_rect.width, background_rect.height = screen.get_size()
